530fd6b4222042a0f27baebdcdd3603c4cc99e5b
ck2rpg <162077340+ck2rpg@users.noreply.github.com>
2024-07-12 00:23:30 -0400

    Automatic flatmap and locators
    Flatmap and locator processes are now automatic.

--- gimpscript.py
+++ gimpscript.py
@@ -39,7 +39,6 @@ def process_map_object_masks(image, save_path):
     new_layer = pdb.gimp_image_get_active_layer(new_image)
     pdb.file_png_save(new_image, new_layer, save_path, save_path, 0, 9, 1, 0, 0, 0, 0)
     pdb.gimp_image_delete(new_image)
-    
 
 def process_texture_image(file_path, folder_path):
     image = pdb.gimp_file_load(file_path, file_path)
@@ -59,7 +58,20 @@ def process_palette_image(file_path, folder_path):
     pdb.file_dds_save(image, layer, save_path, save_path, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0)
     pdb.gimp_image_delete(image)
 
+def process_flatmap_image(folder_path):
+    file_path = os.path.join(folder_path, 'papyrus.png')
+    base_dir = os.path.dirname(folder_path)
+    save_path = os.path.join(base_dir, 'gfx', 'map', 'terrain', 'flatmap.dds')
+    image = pdb.gimp_file_load(file_path, file_path)
+    pdb.plug_in_gauss(image, image.active_layer, 7, 7, 0)
+    pdb.gimp_brightness_contrast(image.active_layer, -127, 0)
+    pdb.file_dds_save(image, image.active_layer, save_path, save_path, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0)
+    pdb.gimp_image_delete(image)
+
 def process_folder(folder_path):
+    # Call the new method to process the flatmap image
+    process_flatmap_image(folder_path)
+    
     for file in os.listdir(folder_path):
         full_path = os.path.join(folder_path, file)
         base_dir = os.path.dirname(folder_path)
@@ -90,8 +102,15 @@ def process_folder(folder_path):
             "gen_religions_l_english.yml": os.path.join('localization', 'english', 'religion'),
             "gen_religions.txt": os.path.join('common', 'religion', 'religions'),
             "gen_holy_sites.txt": os.path.join('common', 'religion', 'holy_sites'),
-	    "gen_hybrid_creation_names.txt": os.path.join('common', 'culture', 'creation_names'),
-	    "gen_hybrid_cultures_l_english.yml": os.path.join('localization', 'english', 'culture')
+            "gen_hybrid_creation_names.txt": os.path.join('common', 'culture', 'creation_names'),
+            "gen_hybrid_cultures_l_english.yml": os.path.join('localization', 'english', 'culture'),
+            "building_locators.txt": os.path.join('gfx', 'map', 'map_object_data'),
+            "combat_locators.txt": os.path.join('gfx', 'map', 'map_object_data'),
+            "other_stack_locators.txt": os.path.join('gfx', 'map', 'map_object_data'),
+            "player_stack_locators.txt": os.path.join('gfx', 'map', 'map_object_data'),
+            "siege_locators.txt": os.path.join('gfx', 'map', 'map_object_data'),
+            "special_building_locators.txt": os.path.join('gfx', 'map', 'map_object_data'),
+            "stack_locators.txt": os.path.join('gfx', 'map', 'map_object_data')
         }
         lower_file = file.lower()
         if lower_file in file_mappings:
@@ -104,5 +123,8 @@ def process_folder(folder_path):
             process_image(full_path, folder_path)
         elif ("color_palette" in lower_file):
             process_texture_image(full_path, folder_path)
+    
+    # Log message to console
+    print("Your Gimp Process is complete. Don't forget to complete the rest of the steps!")
 
 process_folder('C:\\Users\\YOURUSERNAME\\Documents\\Paradox Interactive\\Crusader Kings III\\mod\\YOURMODNAME\\replacers')
